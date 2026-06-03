63b1f378b55851e5a1a2d8d2cb46326645829a3e
looklukelike <somigliluca@gmail.com>
2026-06-03 16:53:40 +0100

    Time: 259 ms (83.35%), Space: 67.4 MB (20.31%) - LeetHub

--- Pandas/0177-nth-highest-salary/0177-nth-highest-salary.py
+++ Pandas/0177-nth-highest-salary/0177-nth-highest-salary.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
     x = None
-    employee = employee.sort_values('salary', ascending=False)['salary'].drop_duplicates()
-    if N > 0 and employee.shape[0] >= N:
-        x = employee.iloc[N - 1]
+    salaries = employee['salary'].sort_values(ascending=False).drop_duplicates()
+    if N > 0 and salaries.shape[0] >= N:
+        x = salaries.iloc[N - 1]
     df = pd.DataFrame({f'getNthHighestSalary({N})': [x]})
     return df
