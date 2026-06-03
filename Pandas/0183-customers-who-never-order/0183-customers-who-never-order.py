94fb64aec3ff95b9a6f975ac23ac1c84212722f5
looklukelike <somigliluca@gmail.com>
2026-06-03 12:38:08 +0100

    Time: 289 ms (43.07%), Space: 68 MB (30.53%) - LeetHub

--- Pandas/0183-customers-who-never-order/0183-customers-who-never-order.py
+++ Pandas/0183-customers-who-never-order/0183-customers-who-never-order.py
@@ -3,5 +3,4 @@ import pandas as pd
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
     df = pd.merge(customers, orders, how='left',left_on='id', right_on='customerId')
     df = df[df['customerId'].isnull()]
-    df = pd.DataFrame(df['name'].rename('Customers'))
-    return df
+    return df[['name']].rename(columns={'name': 'Customers'})
