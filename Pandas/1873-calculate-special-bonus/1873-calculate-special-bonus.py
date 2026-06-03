386e890e74a7c9ff1bbaf1e3298ebd0ad90c29a5
looklukelike <somigliluca@gmail.com>
2026-06-03 14:16:26 +0100

    Time: 358 ms (9.24%), Space: 67.8 MB (57.6%) - LeetHub

--- Pandas/1873-calculate-special-bonus/1873-calculate-special-bonus.py
+++ Pandas/1873-calculate-special-bonus/1873-calculate-special-bonus.py
@@ -1,13 +1,6 @@
 import pandas as pd
 
-def bonus(row):
-    if row['employee_id'] % 2 == 0:
-        return 0
-    elif row['name'].startswith('M'):
-        return 0
-    else:
-        return row['salary']
-
 def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
-    employees['bonus'] = employees.apply(bonus, axis=1)
-    return employees.loc[:, ['employee_id', 'bonus']].sort_values('employee_id')
+    mask = (employees['employee_id'] % 2 != 0) & (~employees['name'].str.startswith('M'))
+    employees['bonus'] = np.where(mask, employees['salary'], 0)
+    return employees[['employee_id', 'bonus']].sort_values('employee_id')
