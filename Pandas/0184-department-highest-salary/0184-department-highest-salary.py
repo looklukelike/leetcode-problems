e76c40ae2bebf3a8ccee7e1c1bd12768a1565c2a
looklukelike <somigliluca@gmail.com>
2026-06-04 12:26:57 +0100

    Time: 387 ms (5.3%), Space: 69 MB (54.83%) - LeetHub

--- Pandas/0184-department-highest-salary/0184-department-highest-salary.py
+++ Pandas/0184-department-highest-salary/0184-department-highest-salary.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
-    highest_salaries = employee.groupby('departmentId', as_index=False).max('salary')
+    highest_salaries = employee.groupby('departmentId')['salary'].max().reset_index()
     employee = employee.merge(highest_salaries, how='inner', on=['departmentId', 'salary']).merge(department, how='left', left_on='departmentId', right_on='id')
     employee = employee.rename(columns={'name_y': 'Department', 'name_x': 'Employee', 'salary' : 'Salary'})[['Department', 'Employee', 'Salary']]
     return employee
