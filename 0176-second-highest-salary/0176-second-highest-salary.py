ea9d95ad135776180f5d775a18b1bb5f462d585d
looklukelike <somigliluca@gmail.com>
2026-06-04 12:24:45 +0100

    Delete 0176-second-highest-salary directory

--- 0176-second-highest-salary/0176-second-highest-salary.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
-    x = None
-    salaries = employee['salary'].sort_values(ascending=False).drop_duplicates()
-    if salaries.shape[0] >= 2:
-        x = salaries.iloc[1]
-    df = pd.DataFrame({f'SecondHighestSalary': [x]})
-    return df
