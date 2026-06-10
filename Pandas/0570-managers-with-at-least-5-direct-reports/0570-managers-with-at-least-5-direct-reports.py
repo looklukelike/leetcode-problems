e0b3127643c66c879da750aa685d372efb24712c
looklukelike <somigliluca@gmail.com>
2026-06-10 13:39:06 +0100

    Time: 284 ms (65.1%), Space: 68.4 MB (92.2%) - LeetHub

--- Pandas/0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.py
+++ Pandas/0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def find_managers(employee: pd.DataFrame) -> pd.DataFrame:
-    top_managers = employee.groupby('managerId').count()
+    top_managers = employee.groupby('managerId')['id'].count()
     top_managers = top_managers.loc[top_managers.values >= 5]
 
     return pd.DataFrame(employee.loc[employee['id'].isin(top_managers.index), 'name'])
