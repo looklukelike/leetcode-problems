f6baaa1f1ea2f907726d262ce59ab39f8c51a91f
looklukelike <somigliluca@gmail.com>
2026-06-11 11:46:48 +0100

    Time: 409 ms (24.81%), Space: 69 MB (20.46%) - LeetHub

--- Pandas/0607-sales-person/0607-sales-person.py
+++ Pandas/0607-sales-person/0607-sales-person.py
@@ -3,5 +3,5 @@ import pandas as pd
 def sales_person(sales_person: pd.DataFrame, company: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
     orders = orders.merge(company, how='left', on='com_id')
     orders = orders.loc[orders['name'] == 'RED', 'sales_id']
-    sales_person = sales_person.loc[~sales_person['sales_id'].isin(orders.values), 'name']
-    return pd.DataFrame(sales_person)
+    sales_person = sales_person.loc[~sales_person['sales_id'].isin(orders.values), ['name']]
+    return sales_person
