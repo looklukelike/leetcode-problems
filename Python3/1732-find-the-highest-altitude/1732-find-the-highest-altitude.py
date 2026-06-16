750456e9afcde24d40cc7343a0fe39c1fb67a994
looklukelike <somigliluca@gmail.com>
2026-06-16 11:40:00 +0100

    Time: 0 ms (100%), Space: 19.1 MB (86.04%) - LeetHub

---
+++ Python3/1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -0,0 +1,7 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        altitude = max_altitude = 0
+        for i in range(len(gain)):
+            altitude += gain[i]
+            max_altitude = max(max_altitude, altitude)
+        return max_altitude
