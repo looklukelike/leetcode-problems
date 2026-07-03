8e217f7d89ea9b23e0ae4c326fc5ad22229b7ea1
looklukelike <somigliluca@gmail.com>
2026-07-03 15:32:31 +0100

    Time: 185 ms (30.75%), Space: 20.6 MB (41.6%) - LeetHub

--- Python3/0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ Python3/0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -8,15 +8,13 @@ class Solution:
             return tot_sum <= h
 
         lo, hi = 1, max(piles)
-        final_k = -1
 
         while lo <= hi:
             k = (lo + hi) // 2
             if can_eat(k):
-                final_k = k
                 hi = k - 1
             else:
                 lo = k + 1
 
-        return final_k
+        return lo
         
