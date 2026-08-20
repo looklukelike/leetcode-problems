05fcd15ae0bd5242746f9047e3107a49623c16bc
looklukelike <somigliluca@gmail.com>
2026-08-20 09:40:05 +0100

    Time: 1014 ms (5.01%), Space: 20.8 MB (7.39%) - LeetHub

---
+++ Python3/2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.py
@@ -0,0 +1,25 @@
+class SmallestInfiniteSet:
+
+    arr = []
+    def __init__(self):
+        self.arr = [x for x in range(1, 10000)]
+
+    def popSmallest(self) -> int:
+        smallest = self.arr[0]
+        self.arr = self.arr[1:]
+        return smallest
+        
+
+    def addBack(self, num: int) -> None:
+        i = 0
+        while num >= self.arr[i]:
+            if num == self.arr[i]:
+                return
+            i += 1
+        self.arr.insert(i, num)
+
+
+# Your SmallestInfiniteSet object will be instantiated and called as such:
+# obj = SmallestInfiniteSet()
+# param_1 = obj.popSmallest()
+# obj.addBack(num)
